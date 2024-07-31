31267a939cc64dc3f5028c51b115fed88b056e32
darshangoswami <goswamidarshan999@gmail.com>
2024-07-31 01:34:53 -0500

    migrate db to postgress

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,15 @@
+import os
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///mydatabase.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
